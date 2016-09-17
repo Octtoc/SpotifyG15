855fcbcbe35dff33d5167cdb5f9881e5d4d8dcf6
Tunfisch <michl.bub@gmx.de>
2016-09-17 15:52:16 +0200

    Add SpotifyApi

--- SpotifyG15/Program.cs
+++ SpotifyG15/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpotifyApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace SpotifyG15
     {
         static void Main(string[] args)
         {
+            Spotify spotify = new Spotify(Spotify.GetOAuth());
+            Responses.CFID cfid = spotify.CFID;
+            string value = "";
+
+            while(value != "q")
+            {
+                try
+                {
+                    Console.WriteLine(spotify.Status.track.album_resource.name);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+                value = Console.ReadLine();
+            }
         }
     }
 }
