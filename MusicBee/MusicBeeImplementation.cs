d88f59228cc01ea9a291dc86cade301a2176bf73
Michael Bubeck <bubeck@ub-software.de>
2016-09-22 10:10:50 +0200

    dynamic Bitmapgeneration

--- MusicBee/MusicBeeImplementation.cs
+++ MusicBee/MusicBeeImplementation.cs
@@ -1,22 +1,56 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Drawing;
-using System.Windows.Forms;
-using System.Collections.Generic;
-using GDIDynamic;
+﻿using GDIDynamic;
 using GDIDynamic.Controls;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace MusicBeePlugin
 {
     public partial class Plugin
     {
-        private System.Timers.Timer timer = new System.Timers.Timer();
+        private PluginInfo about = new PluginInfo();
+        private GDIText artist;
         private GDIDynamic.GDIDynamic gdi;
         private MusicBeeApiInterface mbApiInterface;
-        private PluginInfo about = new PluginInfo();
-        private GDIText title;
-        private GDIText artist;
         private GDIProgressBar progressLength;
+        private System.Timers.Timer timer = new System.Timers.Timer();
+        private GDIText title;
+
+        // MusicBee is closing the plugin (plugin is being disabled by user or MusicBee is shutting down)
+        public void Close(PluginCloseReason reason)
+        {
+        }
+
+        public bool Configure(IntPtr panelHandle)
+        {
+            // save any persistent settings in a sub-folder of this path
+            string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
+
+            // panelHandle will only be set if you set about.ConfigurationPanelHeight to a non-zero value
+            // keep in mind the panel width is scaled according to the font the user has selected
+            // if about.ConfigurationPanelHeight is set to 0, you can display your own popup window
+            if (panelHandle != IntPtr.Zero)
+            {
+                Panel configPanel = (Panel)Panel.FromHandle(panelHandle);
+                Label prompt = new Label();
+                prompt.AutoSize = true;
+                prompt.Location = new Point(0, 0);
+                prompt.Text = "prompt:";
+                TextBox textBox = new TextBox();
+                textBox.Bounds = new Rectangle(60, 0, 100, textBox.Height);
+                configPanel.Controls.AddRange(new Control[] { prompt, textBox });
+            }
+            return false;
+        }
+
+        // return an array of lyric or artwork provider names this plugin supports
+        // the providers will be iterated through one by one and passed to the RetrieveLyrics/ RetrieveArtwork function in order set by the user in the MusicBee Tags(2) preferences screen until a match is found
+        public string[] GetProviders()
+        {
+            return null;
+        }
 
         public PluginInfo Initialise(IntPtr apiInterfacePtr)
         {
@@ -36,7 +70,7 @@ namespace MusicBeePlugin
             about.ReceiveNotifications = (ReceiveNotificationFlags.PlayerEvents | ReceiveNotificationFlags.TagEvents);
             about.ConfigurationPanelHeight = 0;   // height in pixels that musicbee should reserve in a panel for config settings. When set, a handle to an empty panel will be passed to the Configure function
 
-            gdi = new GDIDynamic.GDIDynamic((Bitmap)Image.FromFile(@"C:\temp\test.bmp"));
+            gdi = new GDIDynamic.GDIDynamic(new Bitmap(160, 43));
 
             title = new GDIText(new Rectangle(10, 3, 10, 10), "");
             artist = new GDIText(new Rectangle(10, 12, 0, 0), "");
@@ -54,52 +88,6 @@ namespace MusicBeePlugin
             return about;
         }
 
-        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            progressLength.Progress = mbApiInterface.Player_GetPosition();
-        }
-
-        public bool Configure(IntPtr panelHandle)
-        {
-            // save any persistent settings in a sub-folder of this path
-            string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
-            // panelHandle will only be set if you set about.ConfigurationPanelHeight to a non-zero value
-            // keep in mind the panel width is scaled according to the font the user has selected
-            // if about.ConfigurationPanelHeight is set to 0, you can display your own popup window
-            if (panelHandle != IntPtr.Zero)
-            {
-                Panel configPanel = (Panel)Panel.FromHandle(panelHandle);
-                Label prompt = new Label();
-                prompt.AutoSize = true;
-                prompt.Location = new Point(0, 0);
-                prompt.Text = "prompt:";
-                TextBox textBox = new TextBox();
-                textBox.Bounds = new Rectangle(60, 0, 100, textBox.Height);
-                configPanel.Controls.AddRange(new Control[] { prompt, textBox });
-            }
-            return false;
-        }
-
-        // called by MusicBee when the user clicks Apply or Save in the MusicBee Preferences screen.
-        // its up to you to figure out whether anything has changed and needs updating
-        public void SaveSettings()
-        {
-            // save any persistent settings in a sub-folder of this path
-            string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
-        }
-
-        // MusicBee is closing the plugin (plugin is being disabled by user or MusicBee is shutting down)
-        public void Close(PluginCloseReason reason)
-        {
-
-        }
-
-        // uninstall this plugin - clean up any persisted files
-        public void Uninstall()
-        {
-
-        }
-
         // receive event notifications from MusicBee
         // you need to set about.ReceiveNotificationFlags = PlayerEvents to receive all notifications, and not just the startup event
         public void ReceiveNotification(string sourceFileUrl, NotificationType type)
@@ -108,12 +96,14 @@ namespace MusicBeePlugin
             switch (type)
             {
                 case NotificationType.PluginStartup:
+
                     // perform startup initialisation
                     switch (mbApiInterface.Player_GetPlayState())
                     {
                         case PlayState.Playing:
 
                         case PlayState.Paused:
+
                             // ...
                             break;
                     }
@@ -122,15 +112,18 @@ namespace MusicBeePlugin
                     artist.Text = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.Artist);
                     title.Text = mbApiInterface.NowPlaying_GetFileTag(MetaDataType.TrackTitle);
                     progressLength.Max = mbApiInterface.NowPlaying_GetDuration();
+
                     // ...
                     break;
             }
         }
 
-        // return an array of lyric or artwork provider names this plugin supports
-        // the providers will be iterated through one by one and passed to the RetrieveLyrics/ RetrieveArtwork function in order set by the user in the MusicBee Tags(2) preferences screen until a match is found
-        public string[] GetProviders()
+        // return Base64 string representation of the artwork binary data from the requested provider
+        // only required if PluginType = ArtworkRetrieval
+        // return null if no artwork is found
+        public string RetrieveArtwork(string sourceFileUrl, string albumArtist, string album, string provider)
         {
+            //Return Convert.ToBase64String(artworkBinaryData)
             return null;
         }
 
@@ -142,13 +135,22 @@ namespace MusicBeePlugin
             return null;
         }
 
-        // return Base64 string representation of the artwork binary data from the requested provider
-        // only required if PluginType = ArtworkRetrieval
-        // return null if no artwork is found
-        public string RetrieveArtwork(string sourceFileUrl, string albumArtist, string album, string provider)
+        // called by MusicBee when the user clicks Apply or Save in the MusicBee Preferences screen.
+        // its up to you to figure out whether anything has changed and needs updating
+        public void SaveSettings()
         {
-            //Return Convert.ToBase64String(artworkBinaryData)
-            return null;
+            // save any persistent settings in a sub-folder of this path
+            string dataPath = mbApiInterface.Setting_GetPersistentStoragePath();
+        }
+
+        // uninstall this plugin - clean up any persisted files
+        public void Uninstall()
+        {
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            progressLength.Progress = mbApiInterface.Player_GetPosition();
         }
     }
 }
